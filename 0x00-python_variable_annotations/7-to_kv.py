9852de7779a0063e0e7b992dfa65fcf72c4c957a
Pebong2023 <ebongchris21@gmail.com>
2024-03-15 01:21:34 +0100

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-""" 
-
- Complex types - string and int/float to tuple
-
 """
-from typing import List, Union, Tuple
+Complex types - string and int/float to tuple
+Write typed-annotated function to_kv that takes str k and an int OR float v
+Returns a tuple
+"""
+from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """ Returns a tuple.
     """
-    return (k, v**2)
+    Typed-annotated function
+    to_kv
+    """
+    return (k, v * v)
